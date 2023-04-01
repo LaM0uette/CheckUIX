6459e74effdd12d3f05ee5b67f5a84f09482958a
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-01 17:53:48 +0200

    up

--- js/script.js
+++ js/script.js
@@ -3,14 +3,14 @@ const themeToggleButton = document.getElementById("headerSwitchTheme");
 const darkColors = {
     "--color--h1": "#10191E",
     "--color--h2": "#EBEBEB",
-    "--color--h3": "#B0DB6F",
+    "--color--h3": "#5FBB97",
     "--color--h4": "#7A7A7A"
 };
 
 const lightColors = {
     "--color--h1": "#EBEBEB",
     "--color--h2": "#243842",
-    "--color--h3": "#B0DB6F",
+    "--color--h3": "#5FBB97",
     "--color--h4": "#EBEBEB"
 };
 
