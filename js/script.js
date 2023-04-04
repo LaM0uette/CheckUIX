2f31c732fafde3edea52111726d8e0254fbf0d8d
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-04 14:00:58 +0200

    add modif logo light dark

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const themeToggleButton = document.getElementById("headerSwitchTheme");
-
+const themeToggleButton = document.getElementById("toggleDarkModeSwitch");
+const logo = document.querySelector(".header.logo");
 const darkColors = {
     "--color--h1-first": "#2C3E4A",
     "--color--h1-second": "#687A7F",
@@ -7,7 +7,6 @@ const darkColors = {
     "--color--h2": "#D9DDDF",
     "--color--h3": "#21B2DF",
 };
-
 const lightColors = {
     "--color--h1-first": "#E0E7E8",
     "--color--h1-second": "#BDC6C7",
@@ -15,6 +14,7 @@ const lightColors = {
     "--color--h2": "#2C3E4A",
     "--color--h3": "#21B2DF"
 };
+const storedTheme = localStorage.getItem("theme") || "dark";
 
 function setTheme(theme) {
     const root = document.documentElement;
@@ -30,17 +30,23 @@ function setTheme(theme) {
 function toggleTheme() {
     const currentTheme = localStorage.getItem("theme") || "dark";
     const newTheme = currentTheme === "dark" ? "light" : "dark";
+
     setTheme(newTheme);
+    setLogo(newTheme);
 }
 
-// Appliquer le thème enregistré ou le thème par défaut
-setTheme(localStorage.getItem("theme") || "dark");
+function setLogo(theme) {
+    if (theme === "dark") {
+        logo.setAttribute("src", "Assets/Svg/Logo/LogoLight.svg");
+        logo.setAttribute("alt", "Logo CheckUIX Light");
+    } else {
+        logo.setAttribute("src", "Assets/Svg/Logo/LogoDark.svg");
+        logo.setAttribute("alt", "Logo CheckUIX Dark");
+    }
+}
 
-themeToggleButton.addEventListener("click", function () {
-    const isDark = this.querySelector(".header-icon-moon").style.display !== "none";
+setTheme(storedTheme);
+setLogo(storedTheme);
 
-    this.querySelector(".header-icon-moon").style.display = isDark ? "none" : "block";
-    this.querySelector(".header-icon-sun").style.display = isDark ? "block" : "none";
-
-    toggleTheme();
-});
+themeToggleButton.checked = storedTheme === "light";
+themeToggleButton.addEventListener("change", function () { toggleTheme(); });
