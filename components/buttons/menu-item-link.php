bd1645d6686983ce8e242d365048b51b0155e6e5
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-05 16:39:14 +0200

    card 100% ok

--- components/buttons/menu-item-link.php
+++ components/buttons/menu-item-link.php
@@ -1,6 +1,6 @@
 <style>
     /* Container */
-    .menu-item-link-container {
+    .menu-item-link-container{
         display: flex;
         justify-content: left;
         align-items: center;
@@ -10,13 +10,13 @@
         border-radius: var(--border--radius-h1);
     }
 
-    .menu-item-link-container:hover  {
+    .menu-item-link-container:hover{
         background-color: var(--color--h3);
         border: var(--border--h1) solid var(--color--h3);
         cursor: pointer;
     }
 
-    .menu-item-link-img {
+    .menu-item-link-img{
         margin-left: 0.6vw;
         margin-right: 0.6vw;
         height: 80%;
@@ -32,9 +32,26 @@
     }
 
     /* Title + description */
-    .menu-item-link-title {
+    .menu-item-link-title-container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        width: 100%;
+        height: 95%;
+    }
+
+    .menu-item-link-title-container>p{
+        flex-grow: 1;
+    }
+
+    .menu-item-link-title{
         font-size: var(--font-size--h2);
         color: var(--color--h3);
+    }
+
+    .menu-item-link-container:hover .menu-item-link-title{
+        color: var(--color--h2);
     }
 
     .menu-item-link-description {
@@ -49,7 +66,7 @@
 
     <div class="menu-item-link-vertical-line"></div>
 
-    <div>
+    <div class="menu-item-link-title-container">
         <h2 class="menu-item-link-title"><?php echo ucfirst($svgName); ?></h2>
         <p class="menu-item-link-description">Outil permettant de tester le responsive de son site à l'aide de pré-réglages prêts à l'emploi.</p>
     </div>
