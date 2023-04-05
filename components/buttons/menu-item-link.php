bc8060ca9a976375e64a2e6f18995b7356da29bc
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-05 16:28:57 +0200

    add all elements in card

--- components/buttons/menu-item-link.php
+++ components/buttons/menu-item-link.php
@@ -1,5 +1,6 @@
 <style>
-    .container {
+    /* Container */
+    .menu-item-link-container {
         display: flex;
         justify-content: left;
         align-items: center;
@@ -9,33 +10,49 @@
         border-radius: var(--border--radius-h1);
     }
 
-    .container:hover  {
+    .menu-item-link-container:hover  {
         background-color: var(--color--h3);
         border: var(--border--h1) solid var(--color--h3);
         cursor: pointer;
     }
 
-    img {
+    .menu-item-link-img {
         margin-left: 0.6vw;
         margin-right: 0.6vw;
         height: 80%;
     }
 
     /* Container vertical line */
-    .container-vertical-line{
+    .menu-item-link-vertical-line{
         width: var(--border--h1);
         border-radius: 0.2vmax;
         margin-right: 0.6vw;
         height: 80%;
         background-color: var(--color--h2);
     }
+
+    /* Title + description */
+    .menu-item-link-title {
+        font-size: var(--font-size--h2);
+        color: var(--color--h3);
+    }
+
+    .menu-item-link-description {
+        font-size: var(--font-size--p);
+        color: var(--color--h2);
+    }
 </style>
 
-<div class="container">
+<div class="menu-item-link-container">
     <?php if (!isset($svgName)) { $svgName = ''; } ?>
-    <img src="assets/svg/menuItems/<?php echo $svgName; ?>.svg" alt="<?php echo $svgName; ?> Icon">
+    <img class="menu-item-link-img" src="assets/svg/menuItems/<?php echo $svgName; ?>.svg" alt="<?php echo $svgName; ?> Icon">
 
-    <div class="container-vertical-line"></div>
+    <div class="menu-item-link-vertical-line"></div>
+
+    <div>
+        <h2 class="menu-item-link-title"><?php echo ucfirst($svgName); ?></h2>
+        <p class="menu-item-link-description">Outil permettant de tester le responsive de son site à l'aide de pré-réglages prêts à l'emploi.</p>
+    </div>
 </div>
 
 <script>
