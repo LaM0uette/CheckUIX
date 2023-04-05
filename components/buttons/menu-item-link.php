0395f47f3facc087a292124627ebcb72cb06850a
LaM0uette <96316021+LaM0uette@users.noreply.github.com>
2023-04-05 16:08:23 +0200

    card menu item sans le texte

--- components/buttons/menu-item-link.php
+++ components/buttons/menu-item-link.php
@@ -1,34 +1,41 @@
 <style>
     .container {
         display: flex;
-        justify-content: center;
+        justify-content: left;
         align-items: center;
         width: 35vmax;
         height: 6.5vmax;
-        border: 3px solid var(--color--h2);
-        border-radius: var(--border--radius-big);
+        border: var(--border--h1) solid var(--color--h2);
+        border-radius: var(--border--radius-h1);
     }
 
-    .container:hover {
-        background-color: var(--color--h2);
+    .container:hover  {
+        background-color: var(--color--h3);
+        border: var(--border--h1) solid var(--color--h3);
         cursor: pointer;
     }
 
     img {
-        width: 80%;
+        margin-left: 0.6vw;
+        margin-right: 0.6vw;
         height: 80%;
     }
 
+    /* Container vertical line */
+    .container-vertical-line{
+        width: var(--border--h1);
+        border-radius: 0.2vmax;
+        margin-right: 0.6vw;
+        height: 80%;
+        background-color: var(--color--h2);
+    }
 </style>
 
 <div class="container">
-    <?php
-    if (!isset($svgName)) {
-        $svgName = '';
-    }
-    ?>
-
+    <?php if (!isset($svgName)) { $svgName = ''; } ?>
     <img src="assets/svg/menuItems/<?php echo $svgName; ?>.svg" alt="<?php echo $svgName; ?> Icon">
+
+    <div class="container-vertical-line"></div>
 </div>
 
 <script>
